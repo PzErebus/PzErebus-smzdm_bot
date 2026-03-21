54e11e89b04bfd26b3919715152353da70e1bca4
PzErebus <PzErebus@users.noreply.github.com>
2026-03-21 09:59:33 +0800

    fix: suppress pip root user warning in青龙面板

--- smzdm_ql.py
+++ smzdm_ql.py
@@ -16,8 +16,8 @@ repo_dir = Path(ql_repo_dir, repo_name)
 def main():
     os.system(
         f"cd {str(repo_dir)}; "
-        f"pip3 install -q --upgrade pip; "
-        f"pip3 install -qe .; "
+        f"pip3 install -q --root-user-action=ignore --upgrade pip; "
+        f"pip3 install -q --root-user-action=ignore -e .; "
         f"smzdm-bot"
     )
 
